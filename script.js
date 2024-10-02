d03def733082188db1139218ef937e1115952e07
Ayham1590 <2107020014@students.stamford.edu>
2024-10-02 15:17:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('menuIcon').addEventListener('click', function () {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('show');
+});
